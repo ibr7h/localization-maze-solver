a06e779241ac31d94537ecfd6e6ea8b7bf51136b
M-Niedoba <matthew.niedoba@gmail.com>
2016-07-06 20:43:29 -0400

    changed settings so window isnt 3200x3200px

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 
+mazeName = 'Maze Display'
 maze = cv2.imread("Sample Maze 1.png",cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Maze Display',maze)
+window = cv2.namedWindow(mazeName,0)
+cv2.imshow(mazeName,maze)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
