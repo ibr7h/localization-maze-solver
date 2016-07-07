9ec31bbaafe6b5ea15fa7583a6d422a3cdcf55e9
M-Niedoba <matthew.niedoba@gmail.com>
2016-07-07 19:54:37 -0400

    Added file selection with Tkinter, reorganized into a series of functions

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 import numpy as np
 import cv2
+import Tkinter as Tk
+from tkFileDialog import askopenfilename
+MAZE_NAME = "Maze Display Window"
 
-mazeName = 'Maze Display'
-maze = cv2.imread("Sample Maze 1.png",cv2.IMREAD_GRAYSCALE)
-window = cv2.namedWindow(mazeName,0)
-cv2.imshow(mazeName,maze)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def getUserSelectedFilePath():
+    root = Tk.Tk()
+    root.withdraw()
+    filepath = askopenfilename()
+    return filepath
+
+def getUserSelectedImage():
+    filename = " "
+    while not ".png" in filename :
+        print("Please Select a .png maze image")
+        filename = getUserSelectedFilePath()
+    return cv2.imread(filename)
+
+def setupWindow():
+    image = getUserSelectedImage()
+    window = cv2.namedWindow(MAZE_NAME,0)
+    cv2.imshow(MAZE_NAME,image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+
+setupWindow()
