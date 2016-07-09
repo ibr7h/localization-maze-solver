b88f02f8f4677b45e5eecc601e74e047253f5286
M-Niedoba <matthew.niedoba@gmail.com>
2016-07-09 16:04:59 -0400

    Added more image proccesing functions

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -5,10 +5,26 @@ class ImageProcessor:
     def __init__(self,image):
         self.imageToProcess = image
 
-    def blurImage(amount):
-        return cv2.GaussianBlur(imageToProcess,(5,5),0)
+    def gaussianBlur(self):
+        return cv2.GaussianBlur(self.imageToProcess,(5,5),0)
 
     def getThresholdedImage(self,isInverted):
         threshType = cv2.THRESH_BINARY_INV if isInverted else cv2.THRESH_BINARY
         retval, threshold = cv2.threshold(self.imageToProcess,127,255,threshType)
         return threshold
+
+    def getOtsuBinarizedImage(self,isInverted):
+        image = self.blurImage()
+        threshType = cv2.THRESH_BINARY_INV if isInverted else cv2.THRESH_BINARY
+        retval, threshed = cv2.threshold(image,0,255,threshType + cv2.THRESH_OTSU)
+        return threshed
+
+    def getAdaptiveThreshold(self, image):
+        return cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C ,\
+                                    cv2.THRESH_BINARY,21,0)
+
+    def medianBlur(self):
+        return cv2.medianBlur(self.imageToProcess,9)
+
+    def blur(self):
+        return cv2.blur(self.imageToProcess,9)
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import Tkinter as Tk
+import sys
 from ImageProcessor import ImageProcessor
 from tkFileDialog import askopenfilename
 
@@ -17,12 +18,12 @@ def getUserSelectedImage():
     while not ".png" in filename :
         print("Please Select a .png maze image")
         filename = getUserSelectedFilePath()
-    return cv2.imread(filename)
+    return cv2.imread(filename, 0)
 
 def setupWindow():
     image = getUserSelectedImage()
     imageProcessor = ImageProcessor(image)
-    image = imageProcessor.getThresholdedImage(False)
+    image = imageProcessor.getThresholdedImage(True)
     window = cv2.namedWindow(MAZE_NAME,0)
     cv2.imshow(MAZE_NAME,image)
     cv2.waitKey(0)
