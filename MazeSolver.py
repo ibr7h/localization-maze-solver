31f7cc66775a1d0aefce06c6c4087986f6f4079e
M-Niedoba <matthew.niedoba@gmail.com>
2016-07-14 20:02:27 -0400

    Added a node class

--- MazeSolver.py
+++ MazeSolver.py
@@ -8,3 +8,17 @@ class MazeSolver:
         self.image = proccessedImage
         self.height,width = image.shape[:2]
         self.discoveredNodes = Q.PriorityQueue
+        self.visitedNodes =
+
+class Node:
+    def __init__(self,x,y,prev):
+        self.position = (x,y)
+        self.length = prev.length +1;
+        self.heuristic = MazeSolver.getHeuristic(position)
+        self.previousNode = path
+
+    def get_priority(self,end):
+        return self.length + self.getStraightLine(end)
+
+    def getStraightLine(self,end):
+        return ((end.x-self.x)**2 + (end.y - self.y)**2)**0.5
