d5f4b592ac7bbcf7b4562fb1d769c66caa8ddfd0
ndobs <matthew.niedoba@gmail.com>
2018-03-18 14:25:26 -0400

    generate measurement matrix

--- src/localization_solver.py
+++ src/localization_solver.py
@@ -14,6 +14,7 @@ class LocalizationSolver(object):
 				print "Robot initialized in a wall, retrying"
 				robot = None
 		self.probabilites = self._populate_maze()
+		self.truth_measurements = self._create_measurement_matrix()
 
 
 	def solve():
@@ -28,11 +29,43 @@ class LocalizationSolver(object):
 		probabilites = np.zeros((self.h,self.w))
 		for row in range(h):
 			for col in range(w):
-				if self.image[column,row] != 0:
-					probabilites[column,row] = 1
+				if self.image[row,col] != 0:
+					probabilites[row,col] = 1
 					total += 1.
 		for row in range(h):
 			for col in range(w):
-				probabilites[column,row] /= total
+				probabilites[row,col] /= total
 		return probabilities
+
+	def _create_measurement_matrix(self):
+		#Generate a set of distances to each closest wall for each point in the maze
+		#For use in updating probability of each location
+		measurements = np.zeros((self.h,self.w,4))
+		for row in range(self.h):
+			last_wall = 1
+			#generate left measurements
+			for col in range(self.w)
+				last_wall += -last_wall if self.image[row,col] == 0 else 1 
+				measurements[row,col,0] = last_wall
+			last_wall = 1
+			#generate right measurements
+			for col in range(self.w -1,-1,-1):
+				last_wall += -last_wall if self.image[row,col] == 0 else 1 
+				measurements[row,col,2] = last_wall
+		for col in range(self.w):
+			last_wall = 1
+			#generate up measurements
+			for row in range(self.h)
+				last_wall += -last_wall if self.image[row,col] == 0 else 1 
+				measurements[row,col,1] = last_wall
+			last_wall = 1
+			#generate down measurements
+			for row in range(self.h -1,-1,-1):
+				last_wall += -last_wall if self.image[row,col] == 0 else 1 
+				measurements[row,col,3] = last_wall
+
+		return measurements
+
+
+
 
