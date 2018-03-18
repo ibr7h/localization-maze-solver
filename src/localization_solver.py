e794c10ed2d863ea5824dedd2727a70410d8405c
ndobs <matthew.niedoba@gmail.com>
2018-03-18 14:10:23 -0400

    create probability matrix for monte carlo localization

--- src/localization_solver.py
+++ src/localization_solver.py
@@ -1,4 +1,5 @@
-from Robot import Robot
+from Robot import 
+import numpy as np
 
 class LocalizationSolver(object):
 
@@ -10,14 +11,28 @@ class LocalizationSolver(object):
 			robot = Robot((self.w,self.h))
 			#ensure the robot is initialized in a random location that is not a wall
 			if image[robot.x,robot.y] == 0:
+				print "Robot initialized in a wall, retrying"
 				robot = None
+		self.probabilites = self._populate_maze()
 
 
 	def solve():
+
+
 		#Todo: localize the robot in the maze and find a way out
 		pass
 
 
-	def _populate_maze():
-		#Todo: populate the maze with a point cloud of robots
-		pass
+	def _populate_maze(self):
+		total = 0;
+		probabilites = np.zeros((self.h,self.w))
+		for row in range(h):
+			for col in range(w):
+				if self.image[column,row] != 0:
+					probabilites[column,row] = 1
+					total += 1.
+		for row in range(h):
+			for col in range(w):
+				probabilites[column,row] /= total
+		return probabilities
+
